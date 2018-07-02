8fc7e733fe9b5253c3e122b58cccd22e82fe067b
Frida <FS_Earthbound@hotmail.com>
2018-07-02 13:20:39 +0200

    Added classes

---
+++ NHibernateTest/BusinessLayer/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NHibernateTest.BusinessLayer
+{
+    public class Profile
+    {
+        private string _name;
+        private string _bio;
+        private byte[] _profileImage;
+
+        public string Name { get => _name; set => _name = value; }
+        public string Bio { get => _bio; set => _bio = value; }
+        public byte[] ProfileImage { get => _profileImage; set => _profileImage = value; }
+
+        public Profile() { }
+    }
+}
---
+++ NHibernateTest/BusinessLayer/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NHibernateTest.BusinessLayer
+{
+    public class Project
+    {
+        private int _projectId;
+        private string _projectName;
+        private string _projectLink;
+        private byte[] _previewImage;
+
+        public int ProjectId { get => _projectId; set => _projectId = value; }
+        public string ProjectName { get => _projectName; set => _projectName = value; }
+        public string ProjectLink { get => _projectLink; set => _projectLink = value; }
+        public byte[] PreviewImage { get => _previewImage; set => _previewImage = value; }
+
+        public Project() { }
+
+    }
+}
